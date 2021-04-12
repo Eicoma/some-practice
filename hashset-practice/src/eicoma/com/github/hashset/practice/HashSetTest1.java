cfb61b3e5932ca10c963756fa1d9469a248bd75d
Eicoma <328084834@qq.com>
2021-04-13 00:18:37 +0800

    HashSet的一些练习

---
+++ hashset-practice/src/eicoma/com/github/hashset/practice/HashSetTest1.java
@@ -0,0 +1,33 @@
+package eicoma.com.github.hashset.practice;
+
+/**
+ * hashset底层为数据 + 链表 + 红黑树的结构
+ * hashset初始容量为16，负载因子为0.75，2倍扩容
+ * 即在hashset容量在达到16*0.75==12时扩容为2倍
+ */
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetTest1 {
+    public static void main(String[] args) {
+        HashSet<Student> hashSet = new HashSet<>();
+
+        //通过重写Student类中的hashCode()和equals()方法后，重复的元素不会再添加到hashset中
+        Student stu1 = new Student("zhangsan",23);
+        Student stu2 = new Student("lisi",24);
+        Student stu3 = new Student("wangwu",24);
+        Student stu4 = new Student("wangwu",24);
+
+        hashSet.add(stu1);
+        hashSet.add(stu2);
+        hashSet.add(stu3);
+        hashSet.add(stu4);
+
+        //迭代器打印集合元素
+        Iterator<Student> iterator = hashSet.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+}
