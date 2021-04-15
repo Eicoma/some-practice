99e4ba736b9100b90e3262b1919a31fb4538074f
Eicoma <328084834@qq.com>
2021-04-15 21:41:50 +0800

    BufferedInputStream和BufferedOutputStream的一些练习

--- io-practice/src/eicoma/com/github/io/practice/BufferedDemo1.java
+++ io-practice/src/eicoma/com/github/io/practice/BufferedDemo1.java
@@ -16,7 +16,14 @@ public class BufferedDemo1 {
         //缓冲流使读写过程提速
         BufferedInputStream bis = new BufferedInputStream(new FileInputStream("E:\\BaiduNetdiskDownload\\AE自制特效片段\\星轨.mov"));
         BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("io-practice\\星轨(通过字节缓冲流提速).mov"));
+        //字节缓冲流在底层实际上也是创建了字节数组（该数组的长度为8192）
 
+        /*在底层（数据源硬盘到内存再到目标硬盘）
+        read()从数据源硬盘直接读取8192个字节，存储到内存中的字节缓冲输入流创建的字节数组中
+        再将该字节数组中的元素一个一个地传入内存中的字节缓冲输出流创建的字节数组中
+        最后再通过write()方法将这个数组直接写入到目标硬盘中，极大地节约了读写时间
+        因为内存的运行速度比硬盘要快很多！！
+         */
         int b;
         while((b = bis.read()) != -1){
             bos.write(b);
