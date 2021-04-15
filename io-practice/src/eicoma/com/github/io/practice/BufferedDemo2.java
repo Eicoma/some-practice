84667c35752edd1298992a0f5f117ba7bf9daf7c
Eicoma <328084834@qq.com>
2021-04-15 22:51:04 +0800

    BufferedInputStream和BufferedOutputStream的一些练习

--- io-practice/src/eicoma/com/github/io/practice/BufferedDemo2.java
+++ io-practice/src/eicoma/com/github/io/practice/BufferedDemo2.java
@@ -11,6 +11,11 @@ public class BufferedDemo2 {
         BufferedInputStream bis = new BufferedInputStream(new FileInputStream("E:\\BaiduNetdiskDownload\\AE自制特效片段\\星轨.mov"));
         BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("io-practice\\星轨(通过字节数组使缓冲字节流提速).mov"));
 
+        /*在底层上，和单字节传输的缓冲字节流不同之处在于
+        在内存中字节缓冲输入流的数组与字节缓冲输出流的数组进行字节交换时
+        使用的是长度为1024的字节的数组，而不是一个一个字节进行交换
+        提升了在内存进行字节交换的效率
+         */
         byte[] byteArray = new byte[1024];
         int len;
         while((len = bis.read(byteArray)) != -1){
