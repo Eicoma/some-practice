b36fa4bd955f2f617706247cbe14e5885b8ea7ca
Eicoma <328084834@qq.com>
2021-04-18 11:18:45 +0800

    Thread Foodie类

--- thread-practice/src/eicoma/com/github/thread/practice/Foodie.java
+++ thread-practice/src/eicoma/com/github/thread/practice/Foodie.java
@@ -1,4 +1,32 @@
 package eicoma.com.github.thread.practice;
 
-public class Foodie {
+public class Foodie extends Thread{
+    @Override
+    public void run() {
+        while(true){
+            synchronized(Desk.lock){
+                //判断可吃的鱼香肉丝数量
+                if(Desk.count ==0){
+                    //count为0，结束循环
+                    break;
+                }else{
+                    if(!Desk.flag){
+                        //此时桌子上有鱼香肉丝，吃货线程执行
+                        System.out.println("鱼香肉丝真好吃~~");
+                        //可吃的鱼香肉丝数量减一
+                        Desk.count--;
+                        //唤醒厨师线程
+                        Desk.lock.notifyAll();
+                    }else{
+                        //此时桌子上没有鱼香肉丝，吃货线程休眠
+                        try {
+                            Desk.lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
