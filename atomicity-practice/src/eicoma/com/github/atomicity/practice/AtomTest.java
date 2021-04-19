d38e2c50c1043940b9503b190476282d8299b7bc
Eicoma <328084834@qq.com>
2021-04-19 23:36:43 +0800

    AtomThread

--- atomicity-practice/src/eicoma/com/github/atomicity/practice/AtomTest.java
+++ atomicity-practice/src/eicoma/com/github/atomicity/practice/AtomTest.java
@@ -4,6 +4,7 @@ public class AtomTest {
     public static void main(String[] args) {
         AtomThreadDemo1 atd = new AtomThreadDemo1();
 
+        // 创建100个线程
         for (int i = 0; i < 100; i++) {
             Thread thread = new Thread(atd);
             thread.start();
--- atomicity-practice/src/eicoma/com/github/atomicity/practice/AtomThreadDemo1.java
+++ atomicity-practice/src/eicoma/com/github/atomicity/practice/AtomThreadDemo1.java
@@ -8,6 +8,8 @@ public class AtomThreadDemo1 implements Runnable{
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
+            //通过AtomicInteger类中的incrementAndGet()来保证原子性
+            //count++不能保证原子性
             int count = ac1.incrementAndGet();
             System.out.println("莱纳踹了" + count + "道门" );
         }
