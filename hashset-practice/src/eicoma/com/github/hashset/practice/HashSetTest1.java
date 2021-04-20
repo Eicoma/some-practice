76aaf079ddf9c3170eb351dee9ee564defe40d90
Eicoma <328084834@qq.com>
2021-04-20 10:18:21 +0800

    HashSet

--- hashset-practice/src/eicoma/com/github/hashset/practice/HashSetTest1.java
+++ hashset-practice/src/eicoma/com/github/hashset/practice/HashSetTest1.java
@@ -2,7 +2,7 @@ package eicoma.com.github.hashset.practice;
 
 /**
  * hashset底层为数据 + 链表 + 红黑树的结构
- * hashset初始容量为16，负载因子为0.75，2倍扩容
+ * hashset初始容量为16，加载因子为0.75，2倍扩容
  * 即在hashset容量在达到16*0.75==12时扩容为2倍,即由16扩容至32
  */
 
