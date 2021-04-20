f76a32803945a6447048a756f6b28e835351fb51
Eicoma <328084834@qq.com>
2021-04-21 00:30:36 +0800

    Semaphore

---
+++ thread-practice/src/eicoma/com/github/thread/threadsafty/semaphore/MyRunnable.java
@@ -0,0 +1,19 @@
+package eicoma.com.github.thread.threadsafty.semaphore;
+
+import java.util.concurrent.Semaphore;
+
+public class MyRunnable implements Runnable{
+    private Semaphore semaphore = new Semaphore(2);
+    @Override
+    public void run() {
+        try {
+            semaphore.acquire();
+            System.out.println("获得通行许可");
+            Thread.sleep(2000);
+            System.out.println("归还通行许可");
+            semaphore.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ thread-practice/src/eicoma/com/github/thread/threadsafty/semaphore/SemaphoreTest.java
@@ -0,0 +1,16 @@
+package eicoma.com.github.thread.threadsafty.semaphore;
+
+
+
+public class SemaphoreTest {
+
+    public static void main(String[] args) {
+        MyRunnable myRunnable = new MyRunnable();
+
+        for (int i = 0; i < 100; i++) {
+            Thread thread = new Thread(myRunnable);
+            thread.start();
+        }
+    }
+}
+
