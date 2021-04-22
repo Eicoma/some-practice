b7d994e59fd3f45c78bb63a7286c93e316734237
Eicoma <328084834@qq.com>
2021-04-22 19:51:09 +0800

    ThreadPool

---
+++ thread-practice/src/eicoma/com/github/thread/practice3/MyRunnable.java
@@ -0,0 +1,8 @@
+package eicoma.com.github.thread.practice3;
+
+public class MyRunnable implements Runnable{
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "我在执行");
+    }
+}
---
+++ thread-practice/src/eicoma/com/github/thread/practice3/ThreadPoolDemo1.java
@@ -0,0 +1,28 @@
+package eicoma.com.github.thread.practice3;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolDemo1 {
+    public static void main(String[] args) throws InterruptedException {
+
+        //1,创建一个默认的线程池对象.池子中默认是空的.默认最多可以容纳int类型的最大值.
+        //ExcecutorSetvice 为一个接口，Executors为一个类
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //Executors --- 可以帮助我们创建线程池对象
+        //ExecutorService --- 可以帮助我们控制线程池
+
+        executorService.submit(()->{
+            System.out.println(Thread.currentThread().getName() + "在执行了");
+        });
+
+        //Thread.sleep(2000);
+
+        executorService.submit(()->{
+            System.out.println(Thread.currentThread().getName() + "在执行了");
+        });
+
+        executorService.shutdown();
+    }
+}
+
