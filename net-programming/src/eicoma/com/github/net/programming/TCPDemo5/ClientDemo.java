8bd6740c74c29a7e30e6055c6850d93246022c2b
Eicoma <328084834@qq.com>
2021-05-03 15:11:07 +0800

    TCPDemo

---
+++ net-programming/src/eicoma/com/github/net/programming/TCPDemo5/ClientDemo.java
@@ -0,0 +1,32 @@
+package eicoma.com.github.net.programming.TCPDemo5;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientDemo {
+    public static void main(String[] args) throws IOException {
+        Socket s  = new Socket("169.254.184.191",12000);
+        //创建缓冲字符输入流，从本模块的file.txt中读取数据
+        BufferedReader br = new BufferedReader(new FileReader("net-programming\\ClientDemo.java"));
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+
+        String line;
+        while((line = br.readLine()) != null){
+            bw.write(line);
+            bw.newLine();
+            bw.flush();
+        }
+
+        //调用结束标签
+        s.shutdownOutput();
+
+        BufferedReader brc = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String fankui = null;
+        fankui = brc.readLine();
+        System.out.println("服务器反馈：" + fankui);
+        //释放资源，因为BufferedReader对象不受Socket对象控制，所以也需要关闭
+        br.close();
+        s.close();
+    }
+}
---
+++ net-programming/src/eicoma/com/github/net/programming/TCPDemo5/ServerDemo.java
@@ -0,0 +1,33 @@
+package eicoma.com.github.net.programming.TCPDemo5;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerDemo {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(12000);
+
+        Socket s = ss.accept();
+        //创建一个缓冲字符输入流对象（通过Socket对象来获取输入流）
+        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        //创建一个缓冲字符输出流对象，输出到本模块的file.txt中
+        BufferedWriter bw = new BufferedWriter(new FileWriter("net-programming\\ClientDemoCopy.java",true));
+
+        String line;
+        while((line = br.readLine()) != null){
+            bw.write(line);
+            bw.newLine();
+            bw.flush();
+        }
+
+        //向流中写入反馈数据
+        BufferedWriter bws = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+        bws.write("文件已成功上传至服务器！");
+        bws.newLine();
+        bws.flush();
+        //释放资源时，因为BufferedWriter对象不受ServerSocket对象控制，所以也得关闭
+        bw.close();
+        ss.close();
+    }
+}
