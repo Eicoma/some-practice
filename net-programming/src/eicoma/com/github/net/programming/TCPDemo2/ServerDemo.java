979b7cc9782cd2e1fa4ea78168db076d60e3bad8
Eicoma <328084834@qq.com>
2021-05-03 11:41:07 +0800

    TCPDemo

--- net-programming/src/eicoma/com/github/net/programming/TCPDemo2/ServerDemo.java
+++ net-programming/src/eicoma/com/github/net/programming/TCPDemo2/ServerDemo.java
@@ -1,4 +1,23 @@
 package eicoma.com.github.net.programming.TCPDemo2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerDemo {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(12000);
+
+        Socket s = ss.accept();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String line;
+        while((line = br.readLine()) != null){
+            System.out.println(line);
+        }
+        //释放资源时仅关闭ServerSocket对象即可
+        ss.close();
+    }
 }
