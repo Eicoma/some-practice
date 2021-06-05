44b5f6c5eb1256aac91980bf2ace409e7b3452df
Eicoma <328084834@qq.com>
2021-06-05 17:44:11 +0800

    mybatis-practice

--- mybatis-practice02/src/eicoma/com/github/mapper/UserMapper.java
+++ mybatis-practice02/src/eicoma/com/github/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package eicoma.com.github.mapper;
 
 import eicoma.com.github.bean.User;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -14,4 +16,12 @@ public interface UserMapper {
     //新增操作
     @Insert("INSERT INTO user VALUES(#{id},#{name},#{age})")
     public abstract Integer insert(User user);
+
+    //修改操作
+    @Update("UPDATE user SET name=#{name},age=#{age} WHERE id=#{id}")
+    public abstract Integer update(User user);
+
+    //删除操作
+    @Delete("DELETE FROM user WHERE id=#{id}")
+    public abstract Integer delete(Integer id);
 }
--- mybatis-practice02/src/eicoma/com/github/test/Test01.java
+++ mybatis-practice02/src/eicoma/com/github/test/Test01.java
@@ -53,4 +53,43 @@ public class Test01 {
         sqlSession.close();
         is.close();
     }
+
+    @Test
+    public void update() throws IOException {
+        //1.加载核心配置文件
+        InputStream is = Resources.getResourceAsStream("MyBatisConfig.xml");
+        //2.获取SqlSession工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        //3.获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        //4.获取UserMapper接口的实现类对象
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        //5.调用实现类对象中的方法，接收结果
+        User user = new User(5, "周七七", 35);
+        Integer result = mapper.update(user);
+        //6.处理结果
+        System.out.println(result);
+        //7.释放资源
+        sqlSession.close();
+        is.close();
+    }
+
+    @Test
+    public void delete() throws IOException {
+        //1.加载核心配置文件
+        InputStream is = Resources.getResourceAsStream("MyBatisConfig.xml");
+        //2.获取SqlSession工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        //3.获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        //4.获取UserMapper接口的实现类对象
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        //5.调用实现类对象中的方法，接收结果
+        Integer result = mapper.delete(5);
+        //6.处理结果
+        System.out.println(result);
+        //7.释放资源
+        sqlSession.close();
+        is.close();
+    }
 }
